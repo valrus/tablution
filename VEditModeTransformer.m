05070ec64427a8c69c2959b55f05f3bb27b6fdeb
valrus <imccowan@gmail.com>
2012-06-16 18:25:24 -0700

    Solo mode; always focus last modified note
    Use ` key to toggle solo mode. In solo mode, entering a note makes a
    new "chord" after the focused one with a single note in it and advances
    the focus by one chord.

---
+++ VEditModeTransformer.m
@@ -0,0 +1,13 @@
+//
+//  VEditModeTransformer.m
+//  tablution
+//
+//  Created by Ian Mccowan on 6/13/12.
+//  Copyright (c) 2012 Nuance, Inc. All rights reserved.
+//
+
+#import "VEditModeTransformer.h"
+
+@implementation VEditModeTransformer
+
+@end
